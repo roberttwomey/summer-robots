471e2c8c670e107e8660e7173018f2e2a1eb9683
Robert Twomey <robert.twomey@gmail.com>
2022-06-13 01:02:55 +0200

    added homography calc

--- trace_border_homography.py
+++ trace_border_homography.py
@@ -6,13 +6,19 @@
 from xarm.wrapper import XArmAPI
 import time
 import sys
+import cv2
+import numpy as np
 
 arm = XArmAPI('192.168.4.15')
 arm.connect()
 arm.set_pause_time(0.2)
 time.sleep(3)
 
+
 pauseBetween = False
+simulate = False
+
+arm.set_simulation_robot(on_off=simulate)
 
 rest = [250, 0, 120, 180, 0, 0]
 
@@ -23,6 +29,33 @@ botleft = [557.2, -256.0, 110.1, 180.0, 0.0, 0.0]
 botright = [563.1, 250.0, 110.1, 180.0, 0.0, 0.0]
 topright = [240.7, 250.0, 110.1, 180.0, 0.0, 0.0]
 
+# Create Transform
+points1=np.array([[0.0,0.0], [0.0, 1.0], [1.0, 1.0], [1.0, 0.0]])
+points2=np.array([topleft[:2], botleft[:2], botright[:2], topright[:2]])
+
+# reference https://www.pythonpool.com/cv2-findhomography/
+H, status = cv2.findHomography(points1, points2)
+print(H, status)
+
+def calcTransformed(matrix, p):
+	px = (matrix[0][0]*p[0] + matrix[0][1]*p[1] + matrix[0][2]) / ((matrix[2][0]*p[0] + matrix[2][1]*p[1] + matrix[2][2]))
+	py = (matrix[1][0]*p[0] + matrix[1][1]*p[1] + matrix[1][2]) / ((matrix[2][0]*p[0] + matrix[2][1]*p[1] + matrix[2][2]))
+	p_after = (int(px), int(py))
+	return p_after
+
+def calcArmPos(matrix, p):
+	p_after = calcTransformed(matrix, p)
+	position = [p_after[0], p_after[1], 110.1, 180.0, 0.0, 0.0]
+	return position
+
+mytest = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+for p in mytest:
+	p_after = calcTransformed(H, p)
+	print(p_after)
+
+# print(transformedPoints)
+# exit()
+
 # joint angle locations
 lookdown = [0.4, 5.6, -1.0, 110.1, 2.3, 101.6, -0.1]
 lookforward = [0.0, -45.0, 0.0, 0.0, 0.0, -45.0, 0.0]
@@ -32,26 +65,28 @@ lookforward = [0.0, -45.0, 0.0, 0.0, 0.0, -45.0, 0.0]
 
 thisspeed = 400 #200 #120 # 350
 
-positions = [
-	rest,
-	topleftup, 
-	topleft, 
-	botleft, 
-	botright, 
-	topright,
-	topleft,
-	topleftup,
-]
+# positions = [
+# 	rest,
+# 	topleftup, 
+# 	topleft, 
+# 	botleft, 
+# 	botright, 
+# 	topright,
+# 	topleft,
+# 	topleftup,
+# ]
 
 # go to look
 code = arm.set_servo_angle(angle=lookforward, speed=20, mvacc=500, wait=True, radius=-1.0)
 
-for position in positions:
-	arm.set_position(*position, speed=thisspeed, wait=True)
-	# arm.set_position(*position, speed=speed, radius=0, wait=True)#350)
-	# arm.set_position(*position, speed=speed, radius=0, wait=False)#350)
-	# arm.set_position(*position, speed=speed, wait=True)#350)
+arm.set_position(*rest, speed=thisspeed, wait=True)
+arm.set_position(*topleftup, speed=thisspeed, wait=True)
 
+points = [ (0, 0), (0, 1), (1, 1), (1, 0), (0, 0)]
+
+for point in points:
+	position = calcArmPos(H, point)
+	arm.set_position(*position, speed=thisspeed, wait=True)
 	print("moving to %s" % position)
 
 	print()
